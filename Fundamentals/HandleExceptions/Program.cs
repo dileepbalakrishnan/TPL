0dfe6e1fd18a7896849168d1d10a68393041e716
dileepkrishnan@hotmail.com <dileepkrishnan@hotmail.com>
2019-02-23 20:18:37 +0530

    Demonstrate handling Aggregated exception

---
+++ Fundamentals/HandleExceptions/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace HandleExceptions
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            TaskWithExceptions();
+            Console.WriteLine("Completed");
+            Console.ReadLine();
+        }
+
+        private static void TaskWithExceptions()
+        {
+            try
+            {
+                Task.Factory.StartNew(() => throw new InvalidOperationException());
+                Task.Factory.StartNew(() => throw new ArgumentException());
+            }
+            catch (AggregateException ae)
+            {
+                ae.Handle(e => e is InvalidOperationException || e is ArgumentException);
+            }
+        }
+    }
+}

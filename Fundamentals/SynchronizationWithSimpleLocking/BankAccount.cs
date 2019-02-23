77d50bcf9d65da6f4aff6b8423ddd61896398a38
Dileep Balakrishnan <dileepkrishnan@hotmail.com>
2019-02-23 23:28:24 +0530

    Demonstrate synchronization using a simple lock.

---
+++ Fundamentals/SynchronizationWithSimpleLocking/BankAccount.cs
@@ -0,0 +1,24 @@
+﻿namespace SynchronizationWithSimpleLocking
+{
+    public class BankAccount
+    {
+        private readonly object _synchro = new object();
+        public int Balance { get; set; }
+
+        public void Deposit(int amount)
+        {
+            lock (_synchro)
+            {
+                Balance += amount;
+            }
+        }
+
+        public void Withdraw(int amount)
+        {
+            lock (_synchro)
+            {
+                Balance -= amount;
+            }
+        }
+    }
+}

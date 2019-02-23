45f46ad2b67fd84697903ef236202700fe7f677a
dileepkrishnan@hotmail.com <dileepkrishnan@hotmail.com>
2019-02-23 16:22:47 +0530

    Demonstrate cancellation of task using CancellationTokenSource

---
+++ Fundamentals/TaskCancellation/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskCancellation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cts = new CancellationTokenSource();
+            Task.Factory.StartNew(() => { TheTask(cts.Token); },cts.Token);
+            Console.ReadKey();
+            cts.Cancel();
+            Console.WriteLine("Cancelled");
+            Console.ReadLine();
+        }
+
+        static void TheTask(CancellationToken token)
+        {
+            while (true)
+            {
+                token.ThrowIfCancellationRequested();// Throws an exception and ends the task if it is cancelled.
+                Console.Write(".");
+            }
+        }
+    }
+}

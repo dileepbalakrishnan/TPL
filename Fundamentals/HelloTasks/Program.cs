6485de0bc909f2cb87feb01b8d5a3747359f9e92
dileepkrishnan@hotmail.com <dileepkrishnan@hotmail.com>
2019-02-23 15:55:54 +0530

    Demonstrate starting a new Task

---
+++ Fundamentals/HelloTasks/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace HelloTasks
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Task.Factory.StartNew(DoSomething);
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+
+        private static void DoSomething()
+        {
+            var times = 100;
+            while (times-- > 0)
+                Console.WriteLine("Some work...");
+        }
+    }
+}

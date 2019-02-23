1fbaee0f5be916455c764ffd0a8e9fb75d2dd986
Dileep Balakrishnan <dileepkrishnan@hotmail.com>
2019-02-23 23:39:27 +0530

    Demonstrate synchronization using the Interlocked class.

---
+++ Fundamentals/LockFreeSynchronization/BankAccount.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+
+namespace LockFreeSynchronization
+{
+    public class BankAccount
+    {
+        private int _balance;
+
+        public int Balance
+        {
+            get => _balance;
+            set => _balance = value;
+        }
+
+        public void Deposit(int amount)
+        {
+            Interlocked.Add(ref _balance, amount);
+        }
+
+        public void Withdraw(int amount)
+        {
+            Interlocked.Add(ref _balance, -amount);
+        }
+    }
+}
---
+++ Fundamentals/LockFreeSynchronization/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LockFreeSynchronization
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var bankAccount = new BankAccount();
+            var workers = new List<Task>();
+            for (var i = 0; i < 1000; i++)
+            {
+                workers.Add(Task.Factory.StartNew(() => { bankAccount.Deposit(1000); }));
+                workers.Add(Task.Factory.StartNew(() => { bankAccount.Withdraw(1000); }));
+            }
+            Task.WaitAll(workers.ToArray());
+            Console.WriteLine($"Current Balance {bankAccount.Balance}");
+            Console.ReadLine();
+        }
+    }
+}

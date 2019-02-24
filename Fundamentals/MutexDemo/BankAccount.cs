db3d92bef458551344035e92936ecefbcd3ec35e
Dileep Balakrishnan <dileepkrishnan@hotmail.com>
2019-02-24 11:26:15 +0530

    Demonstrate the usage of Mutex.

---
+++ Fundamentals/MutexDemo/BankAccount.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading;
+
+namespace MutexDemo
+{
+    public class BankAccount
+    {
+        private readonly Mutex _mtx = new Mutex();
+
+        public int Balance { get; set; }
+
+        public void Deposit(int amount)
+        {
+            var lockTaken = false;
+            try
+            {
+                lockTaken = _mtx.WaitOne();
+                Balance += amount;
+            }
+            finally
+            {
+                if (lockTaken)
+                    _mtx.ReleaseMutex();
+            }
+        }
+
+        public void Withdraw(int amount)
+        {
+            var lockTaken = false;
+            try
+            {
+                lockTaken = _mtx.WaitOne();
+                Balance -= amount;
+            }
+            finally
+            {
+                if (lockTaken)
+                    _mtx.ReleaseMutex();
+            }
+        }
+    }
+}

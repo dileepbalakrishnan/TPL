278e55a621bbf370049f35bad2ae955ef9817e90
Dileep Balakrishnan <dileepkrishnan@hotmail.com>
2019-02-24 11:41:05 +0530

    Demonstrate instance management using a named Mutex;

---
+++ Fundamentals/SingleInstanceAppUsingMutext/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace SingleInstanceAppUsingMutext
+{
+    internal class Program
+    {
+        private const string AppName = "SingleInstanceAppUsingMutext.App";
+
+        private static void Main(string[] args)
+        {
+            if (Mutex.TryOpenExisting(AppName, out var mtx))
+            {
+                Console.WriteLine("Another instance of this App is already running.");
+                Console.WriteLine("Press any key to exit.");
+                Console.ReadLine();
+                return;
+            }
+            using (mtx = new Mutex(false, AppName))
+            {
+                Console.WriteLine("App is running...Press any key to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}

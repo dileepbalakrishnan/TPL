56db4b8befc5de4628457ab3e59041ff6c7a9481
Dileep Balakrishnan <dileepkrishnan@hotmail.com>
2019-02-24 12:34:19 +0530

    Demonstrate the usage of ReaderWriterLockSlim.

---
+++ Fundamentals/ReaderWriterLockDemo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ReaderWriterLockDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+
+            ReaderWriterLockSlim rwls = new ReaderWriterLockSlim();
+
+            var t1 = Task.Factory.StartNew(() =>
+            {
+                for (int i = 0; i < 200; i++)
+                {
+                    rwls.EnterWriteLock();
+                    number++;
+                    Console.WriteLine($"Write : {number}.");
+                    Thread.Sleep(50);
+                    rwls.ExitWriteLock();
+                }
+            });
+
+            var t2 = Task.Factory.StartNew(() =>
+            {
+                for (int i = 0; i < 200; i++)
+                {
+                    rwls.EnterReadLock();
+                    Console.WriteLine($"Read : {number}.");
+                    Thread.Sleep(50);
+                    rwls.ExitReadLock();
+                }
+            });
+            Task.WaitAll(t1, t2);
+            Console.WriteLine("Completed.");
+            Console.ReadLine();
+        }
+    }
+}

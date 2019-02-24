fdbe27e13fc20735293f4b17c1d1de2db1e7d2a6
Dileep Balakrishnan <dileepkrishnan@hotmail.com>
2019-02-24 10:02:25 +0530

    Demonstrate the usage of SpinLock

---
+++ Fundamentals/SpinLockDemo/BankAccount.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading;
+
+namespace SpinLockDemo
+{
+    public class BankAccount
+    {
+        private SpinLock sl = new SpinLock();
+
+        public int Balance { get; set; }
+
+        public void Deposit(int amount)
+        {
+            var lockTaken = false;
+            try
+            {
+                sl.Enter(ref lockTaken);
+                Balance += amount;
+            }
+            finally
+            {
+                if (lockTaken)
+                    sl.Exit();
+            }
+        }
+
+        public void Withdraw(int amount)
+        {
+            var lockTaken = false;
+            try
+            {
+                sl.Enter(ref lockTaken);
+                Balance -= amount;
+            }
+            finally
+            {
+                if (lockTaken)
+                    sl.Exit();
+            }
+        }
+    }
+}
---
+++ Fundamentals/SpinLockDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpinLockDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bankAccount = new BankAccount();
+            var workers = new List<Task>();
+            for (var i = 0; i < 1000; i++)
+            {
+                workers.Add(Task.Factory.StartNew(() => { bankAccount.Deposit(1000); }));
+                workers.Add(Task.Factory.StartNew(() => { bankAccount.Withdraw(1000); }));
+            }
+            Task.WaitAll(workers.ToArray());
+            Console.WriteLine($"Current Balance {bankAccount.Balance}");
+            Console.ReadLine();
+        }
+    }
+}

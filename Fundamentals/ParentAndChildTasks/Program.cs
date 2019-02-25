47e05259d48efa05506a96e072488c9333a6b3c0
Dileep Balakrishnan <dileepkrishnan@hotmail.com>
2019-02-25 18:16:05 +0530

    Demonstrate Parent Child relationship.

---
+++ Fundamentals/ParentAndChildTasks/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParentAndChildTasks
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var parent = Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine("Parent task starting...");
+                var child = Task.Factory.StartNew(() =>
+                {
+                    Console.WriteLine("Child task starting...");
+                    Thread.Sleep(1000);
+                    Console.WriteLine("Child task completed.");
+                }, TaskCreationOptions.AttachedToParent);
+            });
+            parent.Wait();
+            Console.WriteLine("Parent task completed.");
+            Console.ReadLine();
+        }
+    }
+}
